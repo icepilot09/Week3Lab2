a42800c5d4696122806fc9806f059d29bf152843
[Michael] <michael@lewiscommunications.com>
2015-09-23 15:00:34 -0500

    finish

--- CreatePromises/createPromiseTemplate.js
+++ CreatePromises/createPromiseTemplate.js
@@ -1,23 +1,42 @@
 var APromise = {};
 
-APromise.all = function(promises){};
+APromise.all = function (promises) {
+    return Promise.all(promises);
+	};
 
-APromise.race = function(promises){
-	function timeout(promise) {
-  return Promise.race([promise.then(function () {
-    throw new Error('Operation timed out');
-  })]);
-}
-};
+APromise.race = function (promises) {
+    return Promise.race(promises);
+	};
 
-APromise.resolve = function(value){
-	var promiseForValue = Promise.resolve(value);
-	return promiseForValue;
-};
+APromise.resolve = function (value) {
+    return Promise.resolve(value);
+	};
 
-APromise.reject = function(err){
-	var rejectedPromise = Promise.reject(new Error('Whatever'));
-	return rejectedPromise;
-};
+APromise.reject = function (err) {
+    return Promise.reject(err);
+	};
 
-module.exports.APromise = APromise;
+
+
+
+
+
+
+
+// var APromise = {};
+
+// APromise.all = function(promises){};
+
+// APromise.race = function(promises){};
+
+// APromise.resolve = function(value){
+// 	var promiseForValue = Promise.resolve(value);
+// 	return promiseForValue;
+// };
+
+// APromise.reject = function(err){
+// 	var rejectedPromise = Promise.reject(new Error('Whatever'));
+// 	return rejectedPromise;
+// };
+
+// module.exports.APromise = APromise;
--- UsePromises/usePromiseTemplate.js
+++ UsePromises/usePromiseTemplate.js
@@ -1,21 +1,99 @@
 function mapAsync(iterator, obj, context) {
-	var data = obj.map(iterator);
-	return Promise.all(data);
+	var result = obj.map(iterator);
+	return Promise.all(result);
 };
 
+
+function mapAsyncWithOrder(iterator, array, context, descending) {
+	var initialValue = Promise.resolve([]);
+	if (!Array.isArray(array)) {
+		return initialValue;
+	}
+
+	iterator = iterator.bind(context);
+	var inOrder = function (prevValue, NextValue, nextIndex, array) {
+		return prevValue.then(function (items) {
+			return iterator(NextValue, nextIndex, array).then(function (moreItems) {
+				return items.concat(moreItems);
+			});
+		});
+	}
+	if (descending) {
+		return array.reduceRight(inOrder, initialValue);
+	}
+	return array.reduce(inOrder, initialValue);
+}
+
 function mapAsyncInOrder(iterator, array, context) {
-	var data = array.map(iterator);
-	return Promise.all(data);
+
+	return mapAsyncWithOrder(iterator, array, context);
+
+
 };
 
 function mapAsyncInDescendingOrder(iterator, array, context) {
-	var data = array.map(iterator);
+
+	return mapAsyncWithOrder(iterator, array, context, true);
+};
+
+
+
+
+
+
+
+
+
+// function mapAsync(iterator, obj, context) {
+// 	var data = obj.map(iterator);
+// 	return Promise.all(data);
+// };
+
+// function mapAsyncWithOrder (iterator, array, context, descending){
+// 	var initValue = Promise.resolve([]);
+// 	if (!Array.isArray(array)){
+// 		return initValue;
+// 	}
+// 	iterator = iterator.bind(context);
+// 	var inOrder = function(prevValue, nextValue, nextIndex, array){
+// 		return prevValue.then(function(items){
+// 			return iterator (nextValue, nextIndex, array).then(function(moreItems){
+// 				return items.concat(moreItems)});
+// 		});
+// 	}
+// 	if (descending){
+// 		return array.reduceRight(inOrder, initValue);
+// 	}
+// 	return array.reduce(inOrder, initValue);
+// };
+
+
+// function mapAsyncInOrder(iterator, array, context) {
+// 	return mapAsyncInOrder(iterator, array, context);
+// };
+
+// function mapAsyncInDescendingOrder(iterator, array, context) {
+// 	return mapAsyncInDescendingOrder(iterator, array, context, true);
+// };
+
+
+
+
+
+
+
+
+
+
+		// var data = array.map(iterator);
+	// function numberDes(a,b){
+	// 	return b-a;
+	// }
 	// var reverse = data.sort(function (a,b){
 	// 	return b-a
 	// });
-	return Promise.all(data);
-	
-};
+	//return Promise.all(data.sort(numberDes));
+
 
 // function mapAsync(iterator, obj, context) {
 // 		var arr = [];
