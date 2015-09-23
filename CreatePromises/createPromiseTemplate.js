d5673a60e8e981e7946dce164ba6e7fad82ba677
[Michael] <michael@lewiscommunications.com>
2015-09-23 17:27:59 -0500

    finish

--- CreatePromises/createPromiseTemplate.js
+++ CreatePromises/createPromiseTemplate.js
@@ -1,11 +1,20 @@
 var APromise = {};
 
 APromise.all = function (promises) {
-    return Promise.all(promises);
+	try {
+		return Promise.resolve(promises);
+		} catch (e) {
+		return Promise.reject(e);
+		}
 	};
 
+
 APromise.race = function (promises) {
-    return Promise.race(promises);
+  	return new Promise(function (resolve, reject) {
+    	promises.forEach(function(value){
+      		Promise.resolve(value).then(resolve, reject);
+    		});
+  		});
 	};
 
 APromise.resolve = function (value) {
@@ -15,6 +24,30 @@ APromise.resolve = function (value) {
 APromise.reject = function (err) {
     return Promise.reject(err);
 	};
+
+module.exports.APromise = APromise;
+
+
+
+
+
+// var APromise = {};
+
+// APromise.all = function (promises) {
+//     return Promise.all(promises);
+// 	};
+
+// APromise.race = function (promises) {
+//     return Promise.race(promises);
+// 	};
+
+// APromise.resolve = function (value) {
+//     return Promise.resolve(value);
+// 	};
+
+// APromise.reject = function (err) {
+//     return Promise.reject(err);
+// 	};
 
 
 
@@ -38,5 +71,3 @@ APromise.reject = function (err) {
 // 	var rejectedPromise = Promise.reject(new Error('Whatever'));
 // 	return rejectedPromise;
 // };
-
-// module.exports.APromise = APromise;
