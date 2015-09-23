289c9c49e05be47ad35b86a6701a2adc050eddb7
[Michael] <michael@lewiscommunications.com>
2015-09-23 12:00:17 -0500

    working through promises

--- CreatePromises/createPromiseTemplate.js
+++ CreatePromises/createPromiseTemplate.js
@@ -2,10 +2,22 @@ var APromise = {};
 
 APromise.all = function(promises){};
 
-APromise.race = function(promises){};
+APromise.race = function(promises){
+	function timeout(promise) {
+  return Promise.race([promise.then(function () {
+    throw new Error('Operation timed out');
+  })]);
+}
+};
 
-APromise.resolve = function(value){};
+APromise.resolve = function(value){
+	var promiseForValue = Promise.resolve(value);
+	return promiseForValue;
+};
 
-APromise.reject = function(err){};
+APromise.reject = function(err){
+	var rejectedPromise = Promise.reject(new Error('Whatever'));
+	return rejectedPromise;
+};
 
 module.exports.APromise = APromise;
--- UsePromises/usePromiseTemplate.js
+++ UsePromises/usePromiseTemplate.js
@@ -1,3 +1,48 @@
-function mapAsync(iterator, obj, context) {};
-function mapAsyncInOrder(iterator, array, context) {};
-function mapAsyncInDescendingOrder(iterator, array, context) {};
+function mapAsync(iterator, obj, context) {
+	var data = obj.map(iterator);
+	return Promise.all(data);
+};
+
+function mapAsyncInOrder(iterator, array, context) {
+	var data = array.map(iterator);
+	return Promise.all(data);
+};
+
+function mapAsyncInDescendingOrder(iterator, array, context) {
+	var data = array.map(iterator);
+	// var reverse = data.sort(function (a,b){
+	// 	return b-a
+	// });
+	return Promise.all(data);
+	
+};
+
+// function mapAsync(iterator, obj, context) {
+// 		var arr = [];
+// 		for (var i = 0; i < obj.length; i++) {
+// 			var collect = iterator.call(context, obj[i],i,obj);
+// 			arr.push(collect);
+// 		}
+// 		return Promise.all(arr)
+// };
+
+// function mapAsyncInOrder(iterator, array, context) {
+// 		var arr = [];
+// 		for (var i = 0; i < array.length; i++) {
+// 			var collect = iterator.call(context, array[i],i,array);
+// 			arr.push(collect);
+// 		}
+// 		return Promise.all(arr)
+// };
+
+// function mapAsyncInDescendingOrder(iterator, array, context) {
+// 		var arr = [];
+// 		var reverse = arr.sort(function (a,b){
+// 		return b-a
+// 		});
+// 		for (var i = 0; i < array.length; i++) {
+// 			var collect = iterator.call(context, array[i],i,array);
+// 			arr.push(collect);
+// 		}
+// 		return Promise.all(reverse)
+// };
